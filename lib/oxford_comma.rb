8cf176a8413c125d68631ee9592e39825a890f0e
Charles Adkins <cpeadkins@gmail.com>
2020-03-11 21:01:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,3 @@
-#def oxford_comma(array)
-#  length = array.length
-#  if length == 1
-#    array.join
-#  elsif length == 2 
-#    array[-2] << "and "
-#    array.join
-#    #array.join(" and ")
-#  else
-#    array[-2] << "and "
-#    array.join
-#  end
-#end
-
 def oxford_comma(array)
   if array.length == 2
   array[-2] << " and "
