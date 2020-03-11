c1ba37fc463d8e32c1a882e02a189922f25d23e9
Charles Adkins <cpeadkins@gmail.com>
2020-03-11 20:13:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
-def oxford_comma(array)
+#def oxford_comma(array)
+#  length = array.length
+#  if length == 1
+#    array.join
+#  elsif length == 2 
+#    array[-2] << "and "
+#    array.join
+#    #array.join(" and ")
+#  else
+#    array[-2] << "and "
+#    array.join
+#  end
+#end
 
+def oxford_comma(array)
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
